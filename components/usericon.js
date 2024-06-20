41fcad2b9d7b95c2b33c2c43e2caa6e79a320acd
constantin leitermann <consti.leitermann@icloud.com>
2024-06-20 17:49:24 +0200

    pre design

--- components/usericon.js
+++ components/usericon.js
@@ -8,14 +8,34 @@ import { SymbolView, SymbolViewProps, SFSymbol } from 'expo-symbols';
 const screenWidth = Dimensions.get('window').width; //full width
 
 const user = {
-    true : ['person.crop.circle.fill', ['white', 'white'], 'palette'],
-    false : ['person.crop.circle.badge.exclamationmark.fill', ['red', 'black'], 'palette']
+    true : ['person.fill', ['black', 'black'], 'palette'],
+    false : ['person.slash.fill', ['red', 'black'], 'palette']
 }
 
 
 const UserIcon = ( {func, loginState}) => {
     console.log('try icon')
     console.log('usericon:', loginState)
+    
+    
+    
+    const iconSize = 35; // Größe des Icons
+
+    const styles = StyleSheet.create({
+        iconbutton: {
+            zIndex: 1,
+            width: iconSize + 5,
+            height: iconSize + 5,
+            alignItems: 'center'
+        },
+        symbol: {
+            width: iconSize,
+            height: iconSize,
+            marginTop: 2,
+        },
+    });
+
+
     return (
         <Pressable style={styles.iconbutton} onPress={() => func()} pointerEvents="auto">
             <SymbolView name={user[loginState][0]} style={styles.symbol} colors={user[loginState][1]} type={user[loginState][2]}/>
@@ -26,21 +46,3 @@ const UserIcon = ( {func, loginState}) => {
 
 export default UserIcon;
 
-const iconSize = 40; // Größe des Icons
-
-const styles = StyleSheet.create({
-    icon: {
-        width: iconSize,
-        height: iconSize,
-        
-    },
-    iconbutton: {
-        zIndex: 1,
-        color: 'black'
-    },
-    symbol: {
-        width: iconSize,
-        height: iconSize,
-        marginTop: 2,
-      },
-});
