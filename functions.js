a9559e9fea5ee78742fa68538d7ce8acd0ac5129
constantin leitermann <consti.leitermann@icloud.com>
2024-08-03 12:18:33 +0200

    .

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as DocumentPicker from 'expo-document-picker';
 import { FlatListComponent } from 'react-native';
-//WHAT
+
 import PopUp from './components/PopUp';
 
 import axios from 'axios'
